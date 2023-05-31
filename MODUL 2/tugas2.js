60eee7aa24e1c9c1fbf5ed6e8b5c15d08c4f1062
dhanirhama <dhanirhama2001@gmail.com>
2023-05-31 11:50:36 +0700

    mengerjakan tugas pada modul 2

---
+++ MODUL 2/tugas2.js
@@ -0,0 +1,15 @@
+// Deklarasi variabel
+var firstName = "Rama Dani";
+var lastName = "Dani";
+var age = 23;
+var isMarried = false;
+var hobbies = ["Membaca", "Bermain musik", "Olahraga", "Main Games"];
+var height = 175.5;
+
+// Menampilkan nilai variabel pada konsol
+console.log("Nama Depan: " + firstName);
+console.log("Nama Belakang: " + lastName);
+console.log("Umur: " + age);
+console.log("Menikah: " + isMarried);
+console.log("Hobi: " + hobbies.join(", "));
+console.log("Tinggi Badan: " + height);
