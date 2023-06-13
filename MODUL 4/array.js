03ffd24177693ab820e5463e207543dbf1b9c903
dhanirhama <dhanirhama2001@gmail.com>
2023-06-13 09:52:59 +0700

    modul 4

---
+++ MODUL 4/array.js
@@ -0,0 +1,9 @@
+let myArray = ["Cokelat", 42.5, 22, true, "Programing"];
+console.log(myArray[0]);
+console.log(myArray[1]);
+console.log(myArray[2]);
+console.log(myArray[3]);
+console.log(myArray[4]);
+console.log(myArray[5]);
+
+console.log("Panjang Nilai myArray adalah : " + myArray + ".");
